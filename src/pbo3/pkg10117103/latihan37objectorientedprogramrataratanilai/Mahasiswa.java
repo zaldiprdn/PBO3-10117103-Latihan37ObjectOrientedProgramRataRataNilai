c87777060222d92615acb094472432a2c7f34178
unknown <zaldip90@gmail.com>
2018-10-09 01:44:32 +0700

    Program Rata Rata Nilai

---
+++ src/pbo3/pkg10117103/latihan37objectorientedprogramrataratanilai/Mahasiswa.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117103.latihan37objectorientedprogramrataratanilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rizaldi
+ *  NAMA  :Rizaldi Perdana Seristian
+    NIM   :10117103   
+    KELAS :IF-3
+ */
+public class Mahasiswa {
+    public int nilaiMhs;
+    public double totalNilaiMhs;
+    Scanner scn = new Scanner(System.in);
+
+    public double HitungTotal(int parNilaiMhs, int parN){
+        for (int i=1; i<=parN; i++){
+            System.out.print("Nilai Mahasiswa ke-"+i+" : ");
+            parNilaiMhs = scn.nextInt();
+            totalNilaiMhs += parNilaiMhs;
+        }
+    return totalNilaiMhs;}
+
+    public double HitungRataRataNilaiMhs(double total, int jmlhMhs) {
+        return total/jmlhMhs;
+        
+    }{
+System.out.println("(Developed by : Rizaldi Perdana Seristian)");
+
+}
+    
+}
---
+++ src/pbo3/pkg10117103/latihan37objectorientedprogramrataratanilai/PBO310117103Latihan37ObjectOrientedProgramRataRataNilai.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117103.latihan37objectorientedprogramrataratanilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rizaldi
+ *  NAMA  :Rizaldi Perdana Seristian
+    NIM   :10117103   
+    KELAS :IF-3
+ */
+public class PBO310117103Latihan37ObjectOrientedProgramRataRataNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mahasiswa mah = new Mahasiswa();
+         Scanner scn = new Scanner(System.in);
+
+         int n;
+         System.out.print("Masukkan Banyaknya Mahasiswa : ");
+         n = scn.nextInt();
+
+         mah.HitungTotal(mah.nilaiMhs, n);
+         mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs, n);
+
+        System.out.println("\nMaka, Rata-rata Nilainya adalah : "+mah.HitungRataRataNilaiMhs(mah.totalNilaiMhs,n));
+        System.out.println("Developed by : Rizki Adam Kurniawan");
+
+    }
+    
+}
